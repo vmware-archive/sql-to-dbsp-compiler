8b4034ee6facb06dfa3bcca0ef0d3cd423ad0374
Leonid Ryzhyk <ryzhyk@gmail.com>
2023-01-16 11:02:20 -0800

    `Serialize`/`Deserialize` impls for `Timestamp`.

--- lib/sqllib/src/timestamp.rs
+++ lib/sqllib/src/timestamp.rs
@@ -5,14 +5,59 @@
 
 use std::ops::Add;
 use size_of::SizeOf;
-use chrono::*;
-use serde::*;
+use chrono::{DateTime, Datelike, NaiveDateTime, TimeZone, Utc};
+use serde::{de::Error as _, ser::Error as _, Deserialize, Deserializer, Serialize, Serializer};
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, SizeOf, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, SizeOf)]
 pub struct Timestamp {
     // since unix epoch
     milliseconds: i64,
 }
+
+/// Serialize timestamp into the `YYYY-MM-DD HH:MM:SS.fff` format, where
+/// `fff` is the fractional part of a second.
+impl Serialize for Timestamp {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let datetime =
+            NaiveDateTime::from_timestamp_millis(self.milliseconds).ok_or_else(|| {
+                S::Error::custom(format!(
+                    "timestamp value '{}' out of range",
+                    self.milliseconds
+                ))
+            })?;
+
+        serializer.serialize_str(&datetime.format("%F %T%.f").to_string())
+    }
+}
+
+/// Deserialize timestamp from the `YYYY-MM-DD HH:MM:SS.fff` format.
+///
+/// # Caveats
+///
+/// * The format does not include a time zone, because the `TIMESTAMP` type is supposed to be
+///   timezone-agnostic.
+/// * Different databases may use different default export formats for timestamps.  Moreover,
+///   I suspect that binary serialization formats represent timestamps as numbers (which is how
+///   they are stored inside the DB), so in the future we may need a smarter implementation that
+///   supports all these variants.
+impl<'de> Deserialize<'de> for Timestamp {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let timestamp_str: &'de str = Deserialize::deserialize(deserializer)?;
+
+        let timestamp = NaiveDateTime::parse_from_str(&timestamp_str, "%F %T%.f").map_err(|e| {
+            D::Error::custom(format!("invalid timestamp string '{timestamp_str}': {e}"))
+        })?;
+
+        Ok(Self::new(timestamp.timestamp_millis()))
+    }
+}
+
 
 impl Timestamp {
     pub const fn new(milliseconds: i64) -> Self {
